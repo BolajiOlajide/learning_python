34617b8a076dce3c2a7db4dcd43bb3920dc743cd
andela-sdamian <simon.peter@andela.com>
2016-07-02 18:13:50 +0100

    learnt the basisc of using strings in python

---
+++ lesson_4/string.py
@@ -0,0 +1,12 @@
+print "hellos world"  # a sample string hello world
+
+print "it's hers "  # mixture of double and quoted string
+
+print "My name is \"simon peter\""  # escaping double quote to be printed
+
+first_name = "Simon Peter"  # assigns the value Simon Peter to variable first_name
+last_name = "Damian"  # assigns the value Damian to last_name
+
+print last_name + first_name  # concatenates two names
+
+print last_name + ' ' + first_name  # concatenates two names adding a space before each
