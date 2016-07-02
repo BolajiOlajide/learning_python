05171a073b505b4cddac0db8a35f04b2e7c97d03
andela-sdamian <simon.peter@andela.com>
2016-07-02 21:43:00 +0100

    learnt some string functions

---
+++ lesson_10/string_stuffs.py
@@ -0,0 +1,25 @@
+greet = "Hello %s how is your %s"
+
+placeholders = ("simon", "thing")
+
+print greet % placeholders  # substitute the value in the string with the placeholders
+
+sentence = "I am a very handsome young man"
+
+print sentence.find("handsome")
+
+my_list = ["simon", "peter"]
+
+glue = " <> "
+
+print glue.join(my_list) # takes the glue var and places it between each list
+
+name = raw_input("Enter you name please: ").lower() #returns name in lower case
+
+print name
+
+my_word = "I love old women"
+
+print my_word.replace('old', 'young')
+
+
