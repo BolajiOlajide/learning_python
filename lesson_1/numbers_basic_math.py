30c56a227193981bf88fe3d2539514a3c2cc495c
andela-sdamian <simon.peter@andela.com>
2016-07-02 17:45:05 +0100

    learnt the basics of carrying out simple arithmetic with python

---
+++ lesson_1/numbers_basic_math.py
@@ -0,0 +1,7 @@
+print(2 + 2)  # adds two numbers
+print(10 - 10)  # subtracts two numbers
+print(5.0 / 2.0)  # divides two floating point numbers
+print(6 * 10)  # multiplies 6 by 10
+print(9 % 2)  # remainder of 9 divided by 4
+print(8.75 % 0.5)  # remainder of 8.75 divided by 0.5
+print(2 ** 3)  # 2 to the power of three
