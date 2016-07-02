309dfef81f6519e56bafab8721bebc4a54c3f732
andela-sdamian <simon.peter@andela.com>
2016-07-02 18:04:25 +0100

    learnt how to use python built in function as well as importing and using modules

---
+++ lesson_3/modules_and_built_in_function.py
@@ -0,0 +1,10 @@
+import math
+
+print pow(5, 2)  # used the built in function to print 5 to the power of 2
+print abs(-10)  # prints the absolute value of -10 which is 10
+
+print math.floor(19.7)  # rounds the number down to the nearest whole number
+print math.ceil(19.8)  # rounds the number up to the nearest whole number
+print math.sqrt(4)  # prints the square root of four
+
+print dir(math)  # lists all the methods in the math module
