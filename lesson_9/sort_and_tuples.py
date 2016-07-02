05f7ea764698bb6e094d08452df34ffb03ef8551
andela-sdamian <simon.peter@andela.com>
2016-07-02 21:10:17 +0100

    learnt abut tuples

---
+++ lesson_9/sort_and_tuples.py
@@ -0,0 +1,13 @@
+numbers = [93, 23, 21, 3, 67, 32]
+
+numbers.sort()  # sorts the list in ascending order
+
+sorted('fedcba')  # sorts the string in alphabetical order
+
+#tuples are the same as list but cannot be change
+
+2, 3, 45, 90, 21  # creates a tuple of numbers
+
+my_tuple = (12, 32, 432, 3, 32) # creates a tuple note you cant add delete only good for calling bak your values
+
+my_tuple[0]
