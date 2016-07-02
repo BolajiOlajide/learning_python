3d369f46ce854d3a3ec069e33706d9cf0cb3fa2c
andela-sdamian <simon.peter@andela.com>
2016-07-02 20:16:11 +0100

    Learnt to add list multiply list and used the in keyword

---
+++ lesson_8/edit_list.py
@@ -0,0 +1,16 @@
+print [1, 2, 3, 4] + [5, 6, 7, 8]  # adds two list
+print 'simon' + 'peter'  # adds two sequence of text
+print 'Hip ' * 2  # prints Hip Hip
+print ['a'] * 10  # creates a ten times in new list
+print [21] * 10  # creates 21 ten time in a new list
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+2 in numbers  # returns True
+
+0 in numbers  # returns False
+
+name = 'simon'
+
+'z' in name  # returns false
+
