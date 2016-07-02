4cca1710fff897d3b95ada7cb43c42cd4b20a682
andela-sdamian <simon.peter@andela.com>
2016-07-02 17:52:13 +0100

    Learnt the usage of python variables by assigning values to them and combining them to do stuffs

---
+++ lesson_2/variables.py
@@ -0,0 +1,9 @@
+x = 10  # assigns the value 10 to the variable x
+
+print(x)  # prints the value of 10 to the console
+
+age = input("How old are you?: ")  # assigns the value entered by a user to the variable age
+
+print "You are", age, "years old"
+
+print "In four years time, you will be", age + 4, "years old."
